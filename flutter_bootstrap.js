63204df36a103ed515c3a4b0c9411578477af5c9
dan <henryparedes.m@gmail.com>
2026-05-29 11:46:54 -0500

    Deploy web with APK download link

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "151443281" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1456114785" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
